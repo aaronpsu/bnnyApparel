6c1bf22330a7c20b8e5648ddadc54e6d817841dd
Nato <aaronromero221@gmail.com>
2025-06-03 13:22:30 +0800

    engterm
    endterm
    
    part 1, wala p ung dalawa. VERY bad code. please
    
    medyo ai hahaha

---
+++ bnnyApparel/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bnnyApparel.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
